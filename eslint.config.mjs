f626a88f3519f00a1c8ff99c737aaa4c583b7a21
saransrini03 <saransrini003@gmail.com>
2025-09-18 10:05:56 +0530

    added rules in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,12 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      // 👇 Add this block
+      "@typescript-eslint/no-explicit-any": "off",
+      // or to only warn instead of error:
+      // "@typescript-eslint/no-explicit-any": "warn",
+    },
   },
 ];
 
